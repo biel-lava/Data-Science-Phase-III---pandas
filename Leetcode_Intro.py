0aa73fabf06a231b0d77f5dc4ed9107b959385f8
biel_lava <ralava@up.edu.ph>
2023-11-14 17:23:01 +0800

    Solved problem 2877

--- Leetcode_Intro.py
+++ Leetcode_Intro.py
@@ -3,5 +3,27 @@ LEETCODE PANDAS INTRO
 Date: 11/14/23
 Notes:
     - This is a dedicated file for playing with introductory pandas problems
-    - Basic progression would be puro easy muna then start dealing with the medium difficulties
+    - Goal is to finish the intro problems to practice lahat nung natutunan ko from chapter 5
 '''
+
+'''
+Problem # 2877: Create a DataFrame from List 
+Date: 11/14/23
+Link: https://leetcode.com/problems/create-a-dataframe-from-list/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+Difficulty: Easy
+Notes:  
+    - Create a dataframe with two columns from a given 2D list
+'''
+
+#'''
+import pandas as pd
+
+student_data = [[1, 15, "2018-22379", "BS CHEM"], [2, 11, "2018-40307", "BS CHEM" ], [3, 11, "2018-82999", "BS CHEM"], [4, 20, "2018-93458", "BS CHEM"]]
+
+col_names = ["student_id", "age", "student_number", "course"]
+
+df = pd.DataFrame(student_data, columns=col_names)
+
+print(df)
+
+#'''
--- generator.py
+++ generator.py
@@ -1,8 +1,8 @@
 #''''
 import random as rd
 
-#test_list = [rd.randint(1, 10) for x in range(10)] # random integer number generator
-test_list = [round((rd.uniform(1.0, 10.0)), 2) for x in range(10)] # random floating point number generator
+test_list = [rd.randint(10000, 100000) for x in range(5)] # random integer number generator
+#test_list = [round((rd.uniform(1.0, 10.0)), 2) for x in range(10)] # random floating point number generator
 test_list.sort()
 print(test_list)
 #'''
